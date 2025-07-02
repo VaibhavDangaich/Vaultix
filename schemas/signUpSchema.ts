d7cf4007252bd2a78ff95c0c69cbc8bb19863870
VaibhavDangaich <31512333+VaibhavDangaich@users.noreply.github.com>
2025-07-02 19:07:00 +0530

    feat: Add password confirmation to signup schema
    Added password confirmation to the signup schema to ensure that the entered passwords match.  The  now includes a  field and a refinement to check for equality with the  field.  A more informative error message is also provided if the passwords do not match.  Additionally, a  file was created.

--- schemas/signUpSchema.ts
+++ schemas/signUpSchema.ts
@@ -1,18 +1,21 @@
 import * as z from 'zod';
 
 export const signUpSchema = z.object({
-  email: z
-    .string()
-    .min(1, { message: "email is required" })
-    .email({ message: "Please enter a valid email!!" }),
-  password: z
-    .string()
-    .min(1, { message: "password is required" })
-    .min(8, { message: "Password should be minimum of 8 characters!" }),
+    email: z
+        .string()
+        .min(1, { message: "email is required" })
+        .email({ message: "Please enter a valid email!!" }),
+    password: z
+        .string()
+        .min(1, { message: "password is required" })
+        .min(8, { message: "Password should be minimum of 8 characters!" }),
     passwordConfirmation: z
-    .string()
+        .string()
         .min(1, { message: "Please confirm your password!" }),
     
     
 })
-.refine(()=>{})
+    .refine((data) => data.password === data.passwordConfirmation, {
+        message: "Password do not match",
+        path: ["passwordConfirmation"]
+    });
